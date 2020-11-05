fcd854b3502ac55711440e6717423cbaf3512e47
yizhenxu <yizhen_xu@brown.edu>
2020-11-04 20:14:13 -0500

    test-change name to TGST

--- R/TGST.R
+++ R/TGST.R
@@ -1,12 +1,12 @@
-#' Create a TVLT Object
+#' Create a TGST Object
 #'
-#' Create a TVLT object, usually used as an input for optimal rule search and ROC analysis. 
+#' Create a TGST object, usually used as an input for optimal rule search and ROC analysis. 
 #' @param Z A vector of true disease status (Viral failure coded as Z=1).
 #' @param S A vector of risk Score.
 #' @param phi Percentage of patients taking viral load test. 
 #' @param method Method for searching for the optimal tripartite rule, options are "nonpar" (default) and "semipar".
 #' @return 
-#' An object of class \code{TVLT}.The class contains 6 slots: phi (percentage of VL tests), Z (true viral failure status), S (risk score), Rules (all possible tripartite rules), Nonparametric (logical indicator of the approach), and FNR.FPR (miclassification rates).
+#' An object of class \code{TGST}.The class contains 6 slots: phi (percentage of VL tests), Z (true viral failure status), S (risk score), Rules (all possible tripartite rules), Nonparametric (logical indicator of the approach), and FNR.FPR (miclassification rates).
 #' @keywords Tripartite rules, FNR, FPR.
 #' @export
 #' @examples
@@ -14,10 +14,10 @@
 #' Z = d$Z # True Disease Status
 #' S = d$S # Risk Score
 #' phi = 0.1 #10% of patients taking viral load test
-#' TVLT( Z, S, phi, method="nonpar")
+#' TGST( Z, S, phi, method="nonpar")
 #' 
 
-TVLT <- function(Z, S, phi, method="nonpar"){
+TGST <- function(Z, S, phi, method="nonpar"){
   
   #consider only complete cases
   data <- cbind(Z,S)
@@ -37,7 +37,7 @@ TVLT <- function(Z, S, phi, method="nonpar"){
     fnr.fpr <-  semipar.fnr.fpr(Z,S,rules[,1],rules[,2])
   }
   colnames(fnr.fpr) <- c("FNR","FPR")#add column names
-  result=new("TVLT", phi=phi, Z=Z, S=S, Rules=rules, Nonparametric=logic, FNR.FPR=fnr.fpr)
+  result=new("TGST", phi=phi, Z=Z, S=S, Rules=rules, Nonparametric=logic, FNR.FPR=fnr.fpr)
   
   return(result)
 }
